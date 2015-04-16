647864b83f59a672cb6a5dcac781c85de3da2085
alexandrap3 <alexandra.portis@yale.edu>
2015-04-16 18:23:28 +0000

    first commit of frequency CMS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
-var feeds = require('../feeds.js');
+
+
 
 module.exports = router;
 
@@ -33,20 +34,101 @@ mongoose.connect('mongodb://' + process.env.IP + '/networks');
 
 
 
+
 // =========================================================
 // =
 // =   DEFINE OUR DATA MODELS
 // =
 
-// Define the data structure of a Phrase model
-// It has width, height, top, and left attributes, which are required to be numbers from 0–100
-// And it has a color attribute, which is optionaland is a string (text)
-// Allowed data types (Number, String, Date...): http://mongoosejs.com/docs/schematypes.html
+// A Frequency has many Phrases.
 
-var Phrase = mongoose.model('Phrase', {
-  preposition: {type: String, required: true},
-  noun: {type: String, required: true},
+var Phrases = new mongoose.Schema({
+  sashatext: {type: String},
+  created: { type : Date, default: Date.now }
 });
+
+var Frequency = mongoose.model('Frequency', {
+  interval: {type: Number, required: true},
+  duration: {type: Number, required: true},
+  privacy: {type: String, required: true},
+  user: {type: String},
+  created: {type: Date, default: Date.now},
+  updated: {type: Date, default: Date.now}, // Last time something was added OR deleted
+  completed: {type: Date},
+  phrases: [Phrases]
+ 
+});
+
+
+
+// =========================================================
+// =
+// =   INTERVAL-BASED PRUNING
+// =
+
+// Call clean() every 5 seconds
+setInterval(clean, 5000);
+
+function clean() {
+  
+  // Find all frequencies (possible improvement: query for frequencies with undefined completion)
+  Frequency.find(function(err, frequencies) {
+    if (err) {
+      console.log('Error finding frequencies to clean', err);
+      return;
+    }
+    
+    // Go through each frequency
+    for (var i = 0; i < frequencies.length; i++) {
+      var frequency = frequencies[i];
+      
+      // Only work with frequencies that aren't completed
+      if (!frequency.completed) {
+        
+        // See if it should be completed now
+        var will_be_completed_at = moment(frequency.created).add(frequency.duration, 'm');
+        if (will_be_completed_at.isAfter(moment())) {
+          console.log("Completing frequency", frequency.id);
+          frequency.completed = Date.now();
+          frequency.save(function(err) {
+            if (err) {
+              console.log('Error completing frequency', frequency, err);
+            }
+          });
+        }
+        
+        else {
+          
+          // See if the last updated date is earlier than now minus the interval
+          var must_be_updated_since = moment().subtract(frequency.interval, 'm');
+          
+          if (moment(frequency.updated).isBefore(must_be_updated_since)) {
+            console.log("Pruning frequency", frequency.id);
+            // Clear the last phrase and save the frequency.
+            // Update the timestamp so this can happen again at the next interval for this frequency.
+            if (frequency.phrases.length == 0) {
+              console.log("-- But frequency has no phrases");
+            }
+            else {
+              frequency.phrases[frequency.phrases.length - 1].sashatext = '';
+            }
+            frequency.updated = Date.now();
+            frequency.save(function(err) {
+              if (err) {
+                console.log('Error cleaning frequency', frequency.id, err);
+              }
+            });
+          }
+          
+        }
+        
+      }
+      
+    }
+    
+  });
+
+}
 
 
 
@@ -64,138 +146,201 @@ var Phrase = mongoose.model('Phrase', {
 
 router.get('/', function(request, response, toss) {
   
+  // TODO: The last frequency usually gets cut off due to scroll bar width
+  
   // When the server receives a request for "/", this code runs
 
-  // Find all the Shape records in the database
-  Phrase.find(function(err, phrases) {
+  // Find all the Frequency records in the database
+  // TODO: Limit to the logged-in user?
+  Frequency.find().sort({'_id': -1}).exec(function(err, frequencies) {
     // This code will run once the database find is complete.
-    // phrases will contain a list (array) of all the phrases that were found.
+    // frequencies will contain a list (array) of all the phrases that were found.
     // err will contain errors if any.
 
     // If there's an error, tell Express to do its default behavior, which is show the error page.
     if (err) return toss(err);
     
-    // The list of shapes will be passed to the template.
-    // Any additional variables can be passed in a similar way (response.locals.foo = bar;)
-    response.locals.phrases = phrases;
+    // Find the most recent frequency. This will always be the one that gets added to.
+    if (frequencies[0]) {
+      var latest_frequency_id = frequencies[0].id;  
+    }
     
-    // Also pass the temperature, wind direction, and next bus arrival.
-    // This can crash if the data hasn't loaded for whatever reason, so toss to Express's error page in that case.
-    try {
-      
-      // Use Moment.js to calculate the next bus arrival time minus the current time,
-      // and display it in english e.g. "in 18 minutes".
-      // http://momentjs.com/docs/#/displaying/from/ (returns difference as an english string e.g. "18 minutes")
-      // http://momentjs.com/docs/#/displaying/difference/ (returns difference in milliseconds)
-      var now = moment();
-      var next_arrival = moment(feeds.bus[0].arrival_at);
-      var arriving_in = next_arrival.from(now);
-      var margin = next_arrival.diff(now) / 1000;
-
-      response.locals.temperature = feeds.weather.main.temp;
-      response.locals.font_size = feeds.weather.main.temp / 2;
-      response.locals.wind_direction = feeds.weather.wind.deg;
-      response.locals.arriving_in = arriving_in;
-      response.locals.margin = margin;
-      response.locals.route = feeds.bus[0].route_id;
-      
+     var frequency_width = 100 / frequencies.length;
+    var total_width = 100;
+    if (frequency_width < 15) {
+      frequency_width = 15;
+      total_width = frequency_width * frequencies.length;
+      frequency_width = 100 / total_width * frequency_width;
     }
-    catch(err) {
-      return toss(err);
-    }
+   
+    // The list of frequencies will be passed to the template.
+    response.locals.frequencies = frequencies;
+    response.locals.latest_frequency_id = latest_frequency_id;
+    response.locals.frequency_width = frequency_width;
+    response.locals.total_width = total_width;
     
     // layout tells template to wrap itself in the "layout" template (located in the "views" folder).
     response.locals.layout = 'layout';
 
     // Render the "home" template (located in the "views" folder).
     response.render('home');
-
+    
   });
   
 });
 
 
+// NEW FREQUENCY
+// /frequency/new
 
+router.get('/frequency/new', function(request, response) {
 
-// SHOW PAGE
-// /show?id=54e2058e85b156d10b064ca0
-// Shows a _single_ phrase
-
-router.get('/show', function(request, response, toss) {
+  // When the server receives a request for "/frequency/new", this code runs
   
-  // When the server receives a request for "/show", this code runs
-  
-  // Find a Phrase with this id
-  Phrase.findOne({_id: request.query.id}, function(err, phrase) {
-    // This code will run once the database find is complete.
-    // phrase will contain the found phrase.
-    // err will contain errors if any (for example, no such record).
+  // Just render a basic HTML page with a form. We don't need to pass any variables.
 
+  response.locals.layout = 'layout';
+  response.render('new_frequency');
+
+});
+
+
+// ABOUT
+// /frequency/new
+
+router.get('/about', function(request, response) {
+
+  // When the server receives a request for "/frequency/new", this code runs
+  
+  // Just render a basic HTML page with a form. We don't need to pass any variables.
+
+  response.locals.layout = 'layout';
+  response.render('about');
+
+});
+
+
+// CREATE FREQUENCY
+// /frequency/create
+
+router.get('/frequency/create', function(request, response, toss) {
+  
+  // TODO: Also delete the latest frequency at this point if it's active (before creating the new one)
+
+  // When the server receives a request for "/frequency/create", this code runs
+  
+  var frequency = new Frequency({
+    interval: request.query.interval,
+    duration: request.query.duration,
+    privacy: request.query.privacy,
+    user: 'TK'
+  });
+  
+  // Now save it to the database
+  frequency.save(function(err) {
+    // This code runs once the database save is complete
+
+    // An err here can be due to validations
     if (err) return toss(err);
     
-    response.locals.phrase = phrase;
-    response.locals.layout = 'layout';
-    response.render('show');
+    // Go to the new phrase screen for this frequency
+    response.redirect('/phrase/new?frequency_id=' + frequency.id)
+    
+  });
+
+});
+
+
+// NEW PHRASE
+// /phrase/new?frequency_id=123
+
+router.get('/phrase/new', function(request, response) {
+
+  response.locals.layout = 'layout';
+  response.locals.frequency_id = request.query.frequency_id;
+  response.render('new_phrase');
+  
+});
+
+
+
+// CREATE PHRASE
+// /phrase/create
+// Normally you get to this page by clicking "Submit" on the /phrase/new page, but
+// you could also enter a URL like the above directly into your browser.
+// frequency_id must be one of the parameters passed
+
+router.get('/phrase/create', function(request, response, toss) {
+  
+  response.locals.layout = 'layout';
+
+  // Find the frequency
+  Frequency.findOne({_id: request.query.frequency_id}, function(err, frequency) {
+    // This code runs once the frequency has been found
+    if (err) return toss(err);
+    
+    // Create the phrase in memory
+    frequency.phrases.push({sashatext: request.query.sashatext});
+    
+    // Update the frequency's timestamp
+    frequency.updated = Date.now();
+    
+    // Save the frequency (also saves the phrase)
+    frequency.save(function(err) {
+      // This code runs once the database save is complete
+  
+      // An err here can be due to validations
+      if (err) return toss(err);
+      
+      // Don't render a "thank you" page; instead redirect to the homepage
+      response.redirect('/');
+      
+    });
     
   });
   
 });
 
 
-
-// NEW PAGE
+// LOGIN
 // /new
 
-router.get('/new', function(request, response) {
+router.get('/login', function(request, response) {
 
   // When the server receives a request for "/new", this code runs
   
   // Just render a basic HTML page with a form. We don't need to pass any variables.
 
   response.locals.layout = 'layout';
-  response.render('new');
+  response.render('login');
   
   // Please see views/new.hbs for additional comments
   
 });
 
+router.get('/deletions', function(request, response) {
 
-
-// CREATE PAGE
-// /create?width=25&height=25&top=25&left=25&color=#ff0000
-// Normally you get to this page by clicking "Submit" on the /new page, but
-// you could also enter a URL like the above directly into your browser.
-
-router.get('/create', function(request, response, toss) {
+  // When the server receives a request for "/new", this code runs
   
-  // When the server receives a request for "/create", this code runs
+  // Just render a basic HTML page with a form. We don't need to pass any variables.
+
+  response.locals.layout = 'deletions';
+  response.render('login');
   
-  response.locals.layout = 'layout';
-
-  // Make a new Phrase in memory, with the parameters that come from the URL 
-  // ?width=25&height=25&top=25&left=25&color=#ff0000
-  // and store it in the shape variable
-  var phrase = new Phrase({
-    preposition: request.query.preposition,
-    noun: request.query.noun,
-  });
+  // Please see views/new.hbs for additional comments
   
-  // Now save it to the database
-  phrase.save(function(err) {
-    // This code runs once the database save is complete
+});
 
-    // An err here can be due to validations
-    if (err) return toss(err);
-    
-    // Otherwise render a "thank you" page
-    response.locals.phrase = phrase;
-    response.render('create');
-    
-    // Alternatively we could just do
-    // response.redirect('/');
-    // to send the user straight to the homepage after saving the new shape
+router.get('/current_user_frequencies', function(request, response) {
 
-  });
+  // When the server receives a request for "/new", this code runs
+  
+  // Just render a basic HTML page with a form. We don't need to pass any variables.
+
+  response.locals.layout = 'current_user_frequencies';
+  response.render('login');
+  
+  // Please see views/new.hbs for additional comments
   
 });
 
@@ -212,3 +357,4 @@ router.get('/about', function(request, response) {
   response.render('about');
   
 });
+
